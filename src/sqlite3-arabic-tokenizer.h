5f54455501e70ab018a5a536179fd59723c64886
Shahriar Nasim Nafi <shahriarnasim.nafi@gmail.com>
2023-01-10 03:18:39 +0600

    Add header file

---
+++ src/sqlite3-arabic-tokenizer.h
@@ -0,0 +1,10 @@
+//
+// Created by Shahriar Nasim Nafi on 18/11/22.
+//
+
+#ifndef SQLITE3_ARABIC_TOKENIZER_SQLITE3_ARABIC_TOKENIZER_H
+#define SQLITE3_ARABIC_TOKENIZER_SQLITE3_ARABIC_TOKENIZER_H
+#include "sqlite3ext.h"
+#endif //SQLITE3_ARABIC_TOKENIZER_SQLITE3_ARABIC_TOKENIZER_H
+
+int sqlite3_sqlitearabictokenizer_init(sqlite3 *db, char **error, const sqlite3_api_routines *api);
