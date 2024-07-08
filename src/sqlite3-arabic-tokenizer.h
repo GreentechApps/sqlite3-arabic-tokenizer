ddd456fb7264b03be147fc0205ebb6eaea3202f7
Shahriar Nasim Nafi <shahriarnasim.nafi@gmail.com>
2024-07-08 20:49:55 +0600

    Fix windows different linkage issue

--- src/sqlite3-arabic-tokenizer.h
+++ src/sqlite3-arabic-tokenizer.h
@@ -4,7 +4,19 @@
 
 #ifndef SQLITE3_ARABIC_TOKENIZER_SQLITE3_ARABIC_TOKENIZER_H
 #define SQLITE3_ARABIC_TOKENIZER_SQLITE3_ARABIC_TOKENIZER_H
-#include "sqlite3ext.h"
-#endif //SQLITE3_ARABIC_TOKENIZER_SQLITE3_ARABIC_TOKENIZER_H
 
-int sqlite3_sqlitearabictokenizer_init(sqlite3 *db, char **error, const sqlite3_api_routines *api);
+#include "sqlite3ext.h"
+
+#ifdef _WIN32
+    #ifdef SQLITE3_ARABIC_TOKENIZER_EXPORTS
+        #define SQLITE3_ARABIC_TOKENIZER_API __declspec(dllexport)
+    #else
+        #define SQLITE3_ARABIC_TOKENIZER_API __declspec(dllimport)
+    #endif
+#else
+    #define SQLITE3_ARABIC_TOKENIZER_API
+#endif
+
+SQLITE3_ARABIC_TOKENIZER_API int sqlite3_sqlitearabictokenizer_init(sqlite3 *db, char **error, const sqlite3_api_routines *api);
+
+#endif //SQLITE3_ARABIC_TOKENIZER_SQLITE3_ARABIC_TOKENIZER_H
